ab3487af917d7338c4b6729f161e3fa41deb7932
Mitchell C <32877602+Mitch-Co@users.noreply.github.com>
2023-03-01 00:40:33 -0500

    Added ability to limit an entries video height.

--- MrScraper.py
+++ MrScraper.py
@@ -8,25 +8,31 @@ import threading
 # List of download names to force download when run
 manualDownload = []
 
-# Global 
+# Global variables
 channelPath = "./DownloadList.csv"
 ytdlpPath = "./yt-dlp.exe"
 archiveFilename = "record.txt"
 storagePath = "./"
-maxParallel = 2
+maxParallel = 1
 
 # Stop spinning threads after exitAfter seconds
-autoAborter = True
-exitAfter = 8 * 3600
+autoAborter = False
+exitAfter = 10 * 3600
 startTime = time.time()
 
-# This will use cookies from a google account in edge
-useEdgeCookies = False
+# Can use cookies from browser
+useBrowserCookies = False
+browserName = "firefox"
+
+# Global video cap - sets max height of video globally
+globalVideoCapFlag = False
+globalVideoCap = 1080
 
 # Safe mode settings
 safeMode = False
 safeStopEvery = 1
 safeStopTime = 1
+
 
 downloadQueue = []
 
@@ -46,10 +52,12 @@ with open(channelPath, newline ='') as toDownload:
             if(row["blockupdate"].lower() != "true"):
                 downloadQueue.append(row)
     except Exception as e:
-        abort("The CSV file contains errors or missing fields" + str(e))
+        abort("Hey dingus the CSV file is messed up" + str(e))
 
 
-def downloadToFolder(name, urlToDownload, audioonly = "", extraargs = ""):
+# Yeah, I define functions right before I need them... 
+# Sue me
+def downloadToFolder(name, urlToDownload, audioonly = "", videolimiter= "", extraargs = ""):
     
     addSlash = ""
     if(storagePath[-1] != "/"):
@@ -61,6 +69,20 @@ def downloadToFolder(name, urlToDownload, audioonly = "", extraargs = ""):
     if(audioonly.lower() == "true"):
         audioFlag = True
 
+    videoLimiterString = ""
+    
+    if (videolimiter != ""):
+        try:
+            temp = int(videolimiter)
+            
+            if(globalVideoCapFlag == True and globalVideoCap > temp):
+                temp = globalVideoCap   
+    
+            videoLimiterString = '[height<=?' + str(temp) +']'
+        
+        except Exception as e:
+            pass
+
 
     if(not os.path.exists(downloadFolder)):
         os.makedirs(downloadFolder)
@@ -69,25 +91,29 @@ def downloadToFolder(name, urlToDownload, audioonly = "", extraargs = ""):
     # All commands have space at the end of them
     dlpCommand = ""
     
+    # I should be using some sort of stringbuilder to not add strings together like this
+    # Sue me
     if(audioFlag):
         dlpCommand += '-o "' + downloadFolder + '/%(title)s [%(id)s].%(ext)s" '
-        dlpCommand += "--extract-audio --audio-format mp3 "
+        dlpCommand += '--extract-audio --audio-format mp3 '
     else:
         dlpCommand += '-o "' + downloadFolder + '/%(title)s [%(id)s]" '
-        dlpCommand += "-f bestvideo+bestaudio " 
+        dlpCommand += '-f "bestvideo' + videoLimiterString + '+bestaudio'
+        if(videoLimiterString != ""):
+            dlpCommand += '/bestvideo+bestaudio'
+        dlpCommand += '" '
         
     dlpCommand += '--download-archive "' + (downloadFolder + "/" + archiveFilename) +  '" '
     dlpCommand += '--no-overwrites '
-    if(useEdgeCookies):
-        dlpCommand += '--cookies-from-browser edge '
-    dlpCommand += extraargs + " "
+    if(useBrowserCookies):
+        dlpCommand += '--cookies-from-browser ' + browserName.lower() + ' '
+    dlpCommand += extraargs + ' '
     dlpCommand += urlToDownload
 
-    print("----------- DOWNLOADING -----------\n" + "Folder Name: " + name + "\n" + "URL: " + urlToDownload + "\n" + "Audio Only: " + str(audioFlag) + "\n" + "Extra Args: " + extraargs + "\n" + "Full Command: yt-dlp.exe " + dlpCommand + "\n\n")
+    print("----------- DOWNLOADING -----------\n" + "Folder Name: " + name + "\n" + "URL: " + urlToDownload + "\n" + "Audio Only: " + str(audioFlag) + "\n" + "Max Video Height: " + videoLimiterString + "\n"  + "Extra Args: " + extraargs + "\n" + "Full Command: yt-dlp.exe " + dlpCommand + "\n\n")
 
     os.system("start /wait cmd /MIN /c yt-dlp.exe " + dlpCommand)
 
-# loops until time on autoAborter runs out or downloadQueue is finished
 safeModeCount = 0
 while len(downloadQueue) > 0:
     if(autoAborter and ((time.time() - startTime) > exitAfter)):
@@ -96,8 +122,9 @@ while len(downloadQueue) > 0:
     if(threading.active_count() - 1 >= maxParallel):
         time.sleep(0.3)
         continue
-    threading.Thread(target=downloadToFolder, args=(downloadQueue[-1]["name"], downloadQueue[-1]["url"], downloadQueue[-1]["audioonly"], downloadQueue[-1]["extraargs"])).start()
+    threading.Thread(target=downloadToFolder, args=(downloadQueue[-1]["name"], downloadQueue[-1]["url"], downloadQueue[-1]["audioonly"], downloadQueue[-1]["videolimiter"], downloadQueue[-1]["extraargs"])).start()
     downloadQueue.pop()
+    
 
     if(safeMode):
         safeModeCount += 1
@@ -107,6 +134,6 @@ while len(downloadQueue) > 0:
         time.sleep(safeStopTime)
 
 
-# Wait for all threads to stop
+# Wait for all threads to stop (not too sure if nessisary?)
 while (threading.active_count() > 1):
     time.sleep(0.3)
--- README.md
+++ README.md
@@ -10,13 +10,26 @@
  
  Entries in the CSV file represent:
 
-  name,audioonly,blockupdate,extraargs,url
+  name,audioonly,blockupdate,videolimiter,extraargs,url
 
   - name is the channel or playlist name
   - audioonly can be TRUE or FALSE: If TRUE, this converts the channel/playlist to a audio format
   - blockupdate can be TRUE or FALSE: If TRUE, this blocks the channel/playlist from updating
+  - videolimiter limits the videos downloaded by the current channel/playlist to a maximum pixel height
   - extraargs should be left blank, but can theoritically be used to append extra commands to the download
   - url is the URL of the channel/playlist
 
+# Examples
   The format of the entries in the csv file should thus be as follows:
-  <File / Channel Name>,<TRUE/FALSE>,<TRUE/FALSE>,"<ARGS APPENDED TO DL COMMAND, CAN BE BLANK>",<URL OF CHANNEL/ PLAYLIST>
+  `<File / Channel Name>,<TRUE/FALSE/BLANK>,<TRUE/FALSE/BLANK>,<Integer/BLANK>, "<ARGS APPENDED TO DL COMMAND/BLANK>",<URL OF CHANNEL/ PLAYLIST>`
+  
+  Some examples of CSV entries include:
+  `Captain Disillusion,,,,,https://www.youtube.com/user/captaindisillusion`
+  `How to Drink,,TRUE,,1080,https://www.youtube.com/c/howtodrink`
+  `valoulette,TRUE,,,,https://www.youtube.com/c/valouletteasmr`
+
+   Do note that the headers in the CSV file included that indicate the field name are nessisary. It is reccomended that you edit the included template file to start.
+  
+	
+	
+  
